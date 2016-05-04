38b54747e4668f297572759547cc670f96f3a211
xLuck <Vedatcetin07@hotmail.com>
2016-05-05 00:40:47 +0300

    Update yeniKullanici.py

--- yeniKullanici.py
+++ yeniKullanici.py
@@ -3,6 +3,7 @@
 import getpass
 from Sifreleme import *
 #Problem : Tum USB ler icin duzeltilmeli A84A-7167!
+#Çözüm: Bir Db içinde bulunan USB bilgileri ve kullanıcı bilgileri karsılaştırma işlemi yapılabilir
 sifrelenmiscikti = ""
 dosya = open("/media/"+getpass.getuser()+"/A84A-7167/information.vsec","w")
 user = input("Kullanici Adi Giriniz:")
