bad66f75797963fb400e07e78522c98df0715ac5
xLuck <Vedatcetin07@hotmail.com>
2016-05-05 00:30:03 +0300

    Update Mail.py

--- Mail.py
+++ Mail.py
@@ -13,4 +13,16 @@ class Mail:
         msg = "\nUYARI!" # GONDERILECEK MESAJ
         server.sendmail("SistemUyari", "vedatcetin07@hotmail.com", msg)
         server.close()
+        
+    def mailyolla(kullaniciBilgi,calisanBilgileri):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+#Next, log in to the server
+        server.login("developerxLuck", "123123xX")
+
+#Send the mail
+        msg = "\nUYARI!\n Kullancı Bilgileri\n"+ kullaniciBilgi +"\n Calısan Program Bilgileri \n"+ calisanBilgileri  # GONDERILECEK MESAJ
+        server.sendmail("SistemUyari", "vedatcetin07@hotmail.com", msg)
+        server.close()
 
