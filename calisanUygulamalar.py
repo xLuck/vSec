6b40828bedd9ebb1c098b7251410698d9ca09c13
xLuck <Vedatcetin07@hotmail.com>
2016-05-11 14:28:10 -0700

    Update calisanUygulamalar.py

--- calisanUygulamalar.py
+++ calisanUygulamalar.py
@@ -1,1 +1,14 @@
 #Sistemde calışan uygulamalar hakkında bilgi edinmek için sınıf
+#Ilk arastirma
+class calisanUygulamalar
+  import os
+  def uygulamalariListele()
+
+    pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+
+    for pid in pids:
+        try:
+            uygulamalar = open(os.path.join('/proc', pid, 'cmdline'), 'rb').read()
+            return uygulamar
+        except IOError: # proc has already terminated
+            continue
